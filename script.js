01b777a754afdc07e765f810d1904cb3170ae9f1
Filip <filipp939@yandex.by>
2022-07-30 13:18:16 +0200

    one prog

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat:false
+};
+const a = prompt('один из последних просмотренных фильмов?',''),
+      b = prompt('на сколько оцените его?',''),
+      c = prompt('один из последних просмотренных фильмов?',''),
+      d = prompt('на сколько оцените его?','');
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;  
+console.log(personalMovieDB);
